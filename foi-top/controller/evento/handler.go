2afd8e1f23ed3d72f1f848424692ebd2e96e9e12
Andreson Thiago <andreson.thiago@gmail.com>
2020-04-17 19:13:21 -0300

    implementando criaçao de evento

--- foi-top/controller/evento/handler.go
+++ foi-top/controller/evento/handler.go
@@ -4,11 +4,9 @@ import (
 	"encoding/json"
 	"github.com/Andreson/go-portifolio/foi-top/controller"
 	event_model "github.com/Andreson/go-portifolio/foi-top/model/evento"
-	user_model "github.com/Andreson/go-portifolio/foi-top/model/usuario"
 	event_service "github.com/Andreson/go-portifolio/foi-top/service/evento"
 	"log"
 	"net/http"
-	"strconv"
 )
 
 func Create(w  http.ResponseWriter, r *http.Request){
@@ -40,27 +38,8 @@ func  toDto(r *http.Request) (event_model.EventoDto,error) {
 	if err != nil {
 		panic(err)
 	}
-
-	owner := data.Data
-
-
-	log.Println("Data recebida ",owner)
-
-	if err!=nil {
-		log.Println("Erro ao converter data do evento ",err)
-		return event_model.EventoDto{},err
-	}
-	userID :=r.Form.Get("userid")
-	idUser, userErr:= strconv.Atoi(userID)
-	if userErr!=nil {
-		log.Println("Erro ao converter parametro Userid",userErr)
-		return event_model.EventoDto{},userErr
-	}
-	return event_model.EventoDto{Titulo: r.Form.Get("titulo"),
-		Endereco:r.Form.Get("endereco"),
-		Bairro: r.Form.Get("bairro"),
-		Usuario:user_model.Usuario{Id: idUser } ,
-		Data: "time.Now()" }, nil
+	 log.Print("Request ",data)
+	   return data, nil
 }
 
 
